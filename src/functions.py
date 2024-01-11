983fe6a370a8533e74ac6102d6d78c9bdeb2d76d
FiocChuck <chasf@google.com>
2024-01-11 16:34:40 -0500

    cors

--- src/functions.py
+++ src/functions.py
@@ -3,19 +3,12 @@ from firebase_admin import credentials, firestore
 
 
 def results():
-    # app = firebase_admin.initialize_app()
-    # db = firestore.client()
 
     if not firebase_admin._apps:
         firebase_admin.initialize_app()
 
-    # app_options = {'projectId': 'cf-data-analytics'}
-    # default_app = firebase_admin.initialize_app(options=app_options)
-
     db = firestore.client()
 
-    # users_ref = db.collection("users")
-    # docs = users_ref.stream()
     cities_ref = db.collection("gemini-demo-images")
     query = cities_ref.order_by(
         "timeStamp", direction=firestore.Query.DESCENDING).limit(3)
--- src/main.py
+++ src/main.py
@@ -4,6 +4,22 @@ from functions import *
 
 @functions_framework.http
 def main(request):
+
+    if request.method == "OPTIONS":
+        # Allows GET requests from any origin with the Content-Type
+        # header and caches preflight response for an 3600s
+        headers = {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET",
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Max-Age": "3600",
+        }
+
+        return ("", 204, headers)
+
+    # Set CORS headers for the main request
+    headers = {"Access-Control-Allow-Origin": "*"}
+
     print("test event")
     output = results()
 
