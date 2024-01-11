91d7001e2bfc9ec42636cb3ccab0a326e35ec3bc
FiocChuck <chasf@google.com>
2024-01-11 15:37:29 -0500

    output dict

--- src/functions.py
+++ src/functions.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 
-def results():
+def results() -> dict:
     # app = firebase_admin.initialize_app()
     # db = firestore.client()
 
