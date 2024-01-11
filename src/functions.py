848721a449ee07a5aead58c4a703b0051488bb9d
FiocChuck <chasf@google.com>
2024-01-11 14:46:21 -0500

    print results

--- src/functions.py
+++ src/functions.py
@@ -4,8 +4,12 @@ from firebase_admin import firestore
 app = firebase_admin.initialize_app()
 db = firestore.client()
 
-users_ref = db.collection("users")
-docs = users_ref.stream()
+# users_ref = db.collection("users")
+# docs = users_ref.stream()
+cities_ref = db.collection("gemini-demo-images")
+query = cities_ref.order_by(
+    "timeStamp", direction=firestore.Query.DESCENDING).limit(1)
+results = query.stream()
 
-for doc in docs:
+for doc in results:
     print(f"{doc.id} => {doc.to_dict()}")
